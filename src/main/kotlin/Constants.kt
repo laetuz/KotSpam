5aff8c4f2b962134b23602f174916f3371c233f9
laetuz <laetuzg@gmail.com>
2025-06-29 23:26:30 +0700

    feat: Add the quick version to send messages.

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,6 @@
+package id.neotica
+
+const val defaultMessage = "this is a sample " +
+        "to type so ill just leave it here"
+
+const val defaultCount = 10
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
 package id.neotica
 
 import java.awt.Robot
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 import java.awt.event.KeyEvent
+import java.util.Locale
 
 fun main() {
-    val defaultMessage = ""
-    val defaultCount = 0
 
     println("Enter 1 if you want to send default message.")
     println("Enter the message to be sent: ")
@@ -19,35 +20,92 @@ fun main() {
             return
         }
         "1" -> {
-            println("one")
-            return
+            val message2 = defaultMessage
+            val count = defaultCount
+
+            sendMessageQuick(
+                message = message2,
+                count = count,
+                robot = robot
+            )
         }
         else -> {
             println("Enter the number of times to send: ")
             val count = readlnOrNull()?.toIntOrNull() ?: 0
-            println("Here is your message: $message $count")
-            if (count <= 0) {
-                println("Invalid count.")
-                return
-            }
 
-            println("Starting in 5 seconds, switch to your target window now.")
-            for (i in 5 downTo 1) {
-                println("$i...")
-                Thread.sleep(1000)
-            }
-            println("Starting spam!")
+            println("Use typing simulation? (y/n): ")
+            val typing = readlnOrNull()?: ""
 
-            for (i in 1..count) {
-                typeBot(robot, message)
-                robot.keyPress(KeyEvent.VK_ENTER)
-                robot.keyRelease(KeyEvent.VK_ENTER)
-                Thread.sleep(100)
+            when (typing) {
+                "y" -> {
+                    sendMessageWithTyping(
+                        message = message,
+                        count = count,
+                        robot = robot
+                    )
+                }
+                "n" -> {
+                    sendMessageQuick(
+                        message = message,
+                        count = count,
+                        robot = robot
+                    )
+                }
+                "" -> {
+                    println("Wrong input.")
+                    return
+                }
             }
-
-            println("Finished spamming!")
         }
     }
+}
+
+fun sendMessageWithTyping(message: String, count: Int, robot: Robot) {
+    println("Here is your message: $message $count")
+    if (count <= 0) {
+        println("Invalid count.")
+        return
+    }
+
+    println("Starting in 5 seconds, switch to your target window now.")
+    for (i in 5 downTo 1) {
+        println("$i...")
+        Thread.sleep(1000)
+    }
+    println("Starting spam!")
+
+    for (i in 1..count) {
+        typeBot(robot, message)
+        robot.keyPress(KeyEvent.VK_ENTER)
+        robot.keyRelease(KeyEvent.VK_ENTER)
+        Thread.sleep(100)
+    }
+
+    println("Finished spamming!")
+}
+
+fun sendMessageQuick(message: String, count: Int, robot: Robot) {
+    println("Here is your message: $message $count")
+    if (count <= 0) {
+        println("Invalid count.")
+        return
+    }
+
+    println("Starting in 5 seconds, switch to your target window now.")
+    for (i in 5 downTo 1) {
+        println("$i...")
+        Thread.sleep(1000)
+    }
+    println("Starting spam!")
+
+    for (i in 1..count) {
+        pasteBot(robot, message)
+        robot.keyPress(KeyEvent.VK_ENTER)
+        robot.keyRelease(KeyEvent.VK_ENTER)
+        Thread.sleep(100)
+    }
+
+    println("Finished spamming!")
 }
 
 fun typeBot(robot: Robot, input: String) {
@@ -68,6 +126,27 @@ fun typeBot(robot: Robot, input: String) {
         if (isUpperCase || requireShift) robot.keyRelease(KeyEvent.VK_SHIFT)
 
         robot.delay(10)
+    }
+}
+
+fun pasteBot(robot: Robot, input: String) {
+    val stringSelection = StringSelection(input)
+    val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+
+    clipboard.setContents(stringSelection, stringSelection)
+
+    val os = System.getProperty("os.name").lowercase(Locale.getDefault())
+    val pasteKey = if (os.contains("mac")) {
+        KeyEvent.VK_META
+    } else {
+        KeyEvent.VK_CONTROL
+    }
+
+    robot.apply {
+        keyPress(pasteKey)
+        keyPress(KeyEvent.VK_V)
+        keyRelease(KeyEvent.VK_V)
+        keyRelease(pasteKey)
     }
 }
 
